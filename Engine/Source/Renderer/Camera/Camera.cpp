21590fcb271e8e86bdb3a0608c04ba991263b90b
Infiteri <infiterilol@gmail.com>
2023-09-21 18:08:52 +0300

    Fixed camera issue regarding the new transform system

--- Engine/Source/Renderer/Camera/Camera.cpp
+++ Engine/Source/Renderer/Camera/Camera.cpp
@@ -16,8 +16,8 @@ namespace Core
 
     Matrix4 Camera::GetViewMatrix()
     {
-        Matrix4 translation = Matrix4::Translate(&position);
-        Matrix4 rotationMatrix = Matrix4::RotationZYX(&rotation);
+        Matrix4 translation = Matrix4::Translate(transform.GetPosition());
+        Matrix4 rotationMatrix = Matrix4::RotationZYX(transform.GetRotation());
         Matrix4 mix = Matrix4::Multiply(&translation, &rotationMatrix);
         return Matrix4::Invert(&mix);
     }
